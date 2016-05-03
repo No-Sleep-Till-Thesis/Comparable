f676a203fdeed6b20ca19e5f49de93f7efa06afc
Justin <justinminh225@gmail.com>
2016-05-02 23:50:25 -0700

    (feat) Added basic react-d3 scatterplot skeleton with dummmy data

--- src/components/posts_index.jsx
+++ src/components/posts_index.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Chart from './chart';
 
 export default class Test extends Component {
   render() {
     console.log(this.context);
+    console.log(Chart);
     return (
       <div>
         <h1> Hello, world! </h1>
+        <Chart />
       </div>
-    )
+    );
   }
 }
--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import Offers from './job_offers';
 import Auth from './Auth_Reducers';
+import DummyData from './dummy_data';
 
 const rootReducer = combineReducers({
   offers: Offers,
-  auth: Auth
+  auth: Auth,
+  DummyData: DummyData
 });
 
 export default rootReducer;
