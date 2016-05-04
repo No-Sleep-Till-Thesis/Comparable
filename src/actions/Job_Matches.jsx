d22a111539f3ebe2710322963d2718ed6f542e1b
Justin <justinminh225@gmail.com>
2016-05-04 00:32:21 -0700

    (feat) created an action function returns matching jobs

--- src/actions/Job_Matches.jsx
+++ src/actions/Job_Matches.jsx
@@ -1,8 +1,16 @@
+import axios from 'axios';
 export const JOB_MATCH = "JOB_MATCH";
 
-export function sendJob(array) {
+export function sendJob(title) {
+  const request = axios.get('/api/jobs', {
+    params: {
+      title: title
+    }
+  });
+  console.log('inside jobMatch');
+  console.log(request);
   return {
     type: JOB_MATCH,
-    payload: response.data
+    payload: request
   };
 }
