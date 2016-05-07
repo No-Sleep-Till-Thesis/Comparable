057e85cd791800f816347d582c3fdc15cda7274f
Justin <justinminh225@gmail.com>
2016-05-06 20:00:34 -0700

    (style) added comments for schemas

--- server/db/model.js
+++ server/db/model.js
@@ -3,12 +3,13 @@ var r = thinky.r;
 var type = thinky.type;
 
 var User = thinky.createModel('User', {
-  id: type.string();
-  email: type.string()
+  email: type.string();
+  authID: type.string();
 });
 
 var Offer = thinky.createModel('Offer', {
   id: type.string();
+  userId: 'refenceAuthID',
   title: type.string();
   location: type.string();
   salary: type.string();
@@ -16,4 +17,10 @@ var Offer = thinky.createModel('Offer', {
   createdAt: type.date().default(r.now())
 });
 
-User.hasMany(Offer, 'offers', 'id', 'userId');
+User.hasMany(Offer, 'offers', 'authOId', 'userId');
+
+
+
+// auth id, email - get both from authO
+
+// offer has its own id and and a key called userId -  userId reference authID
